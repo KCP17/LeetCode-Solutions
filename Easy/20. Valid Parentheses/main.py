880ab0c357e84609f1bbd060d59666c00f826a63
KCP17 <148914885+KCP17@users.noreply.github.com>
2024-05-04 11:49:19 +0700

    Create main.py

---
+++ Easy/20. Valid Parentheses/main.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        for p in s:
+            if stack and ((p == ')' and stack[-1] == '(') or (p == ']' and stack[-1] == '[') or (p == '}' and stack[-1] == '{')):
+                stack.pop()
+            else:
+                stack.append(p)
+        
+        return True if not stack else False
